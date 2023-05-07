f6a93cd61a50f9cde527a67780cdb4305827f7e7
nakazaaa <znaka791@gmail.com>
2023-05-08 01:48:03 +0900

    [Wip]todo編集追加

--- src/pages/todo/[todo_id]/index.tsx
+++ src/pages/todo/[todo_id]/index.tsx
@@ -1,13 +1,16 @@
 
 import {Layout} from "@/components/ui/layout/Layout"
-import {useEffect, useState} from "react";
-import customAxios from "@/lib/customAxios";;
+import {SetStateAction, useEffect, useState} from "react";
+import customAxios from "@/lib/customAxios";
 import Box from "@mui/material/Box";
 import { useRouter } from 'next/router'
-import {Button, Paper, TextField} from "@mui/material";
+import {Button, DialogTitle, Paper, Stack, TextField} from "@mui/material";
 import Grid from "@mui/material/Grid";
 import * as React from "react";
 import Typography from "@mui/material/Typography";
+import Dialog from "@mui/material/Dialog";
+import Input from "@mui/material/Input";
+import {number} from "prop-types";
 
 export default function index() {
     const [title,setTitle] =useState('')
@@ -23,10 +26,45 @@ export default function index() {
             setStatus(response.data.todo.status)
             setTime(response.data.todo.time)
         })
-    },[title,text])
+    },[])
+    const handleChangeTitle = (event: { target: { value: SetStateAction<string> } }) => {
+        setTitle(event.target.value);
+    };
+
+    const handleChangeText = (event: { target: { value: SetStateAction<string> } }) => {
+        setText(event.target.value);
+    };
+
+    const handleChangeTime = (event: { target: { value: SetStateAction<number> } }) => {
+        setTime(event.target.value);
+        return time;
+    };
+    const [IsDialogOpen, setIsDialogOpen] = useState(false)
+
+    const OpenDialog = () => {
+        setIsDialogOpen(true)
+    }
+
+    const CloseDialog = () => {
+        setIsDialogOpen(false)
+    }
+
+    const EditTodo = () => {
+        const PostData = {title:title,text:text,time:time,status:0}
+        const url = process.env.NEXT_PUBLIC_API_HOST+'/api/todo/'+todo_id
+        customAxios.post(url,PostData)
+            .then(function (response) {
+                console.log(response);
+                setIsDialogOpen(false)
+                router.push('/todo');
+                // window.location.reload()
+            })
+            .catch(function (error) {console.log(error);});
+    }
+
   return (
       <Layout>
-
+          <Button sx={{ margin: 2 }} onClick={OpenDialog}>編集</Button>
               <Box sx={{ marginTop: 8 }}>
                   <Paper
                       elevation={3}
@@ -38,7 +76,6 @@ export default function index() {
                       }}
                   >
                       <Grid container spacing={2} justifyContent="center" alignItems="center">
-
                           <Grid item xs={12}>
                               <Typography variant="h6" gutterBottom>タイトル</Typography>
                               {title}
@@ -58,7 +95,45 @@ export default function index() {
                       </Grid>
                   </Paper>
               </Box>
-
+          <Grid item xs={8}>
+              <Dialog open={IsDialogOpen}>
+                  <DialogTitle bgcolor="secondary">
+                      Todo追加
+                  </DialogTitle>
+                  <Stack >
+                      <Typography sx={{ margin: 1 }} variant="h6" gutterBottom>タイトル</Typography>
+                      <TextField sx={{ margin: 2 }}
+                                 id="outlined-basic"
+                                 variant="outlined"
+                                 value={title}
+                                 onFocus={(event) => event.target.select()} // select()を呼ぶことで、フォーカス時に文字列が自動的に選択されるようになる。
+                                 onChange={handleChangeTitle}
+                      />
+                      <Typography sx={{ margin: 1 }} variant="h6" gutterBottom>内容</Typography>
+                      <TextField
+                          sx={{ margin: 2 }}
+                          id="filled-textarea"
+                          multiline
+                          variant="filled"
+                          rows={4}
+                          value={text}
+                          onChange={handleChangeText}
+                      />
+                      <Typography sx={{ margin: 1 }} variant="h6" gutterBottom>時間</Typography>
+                      <Input
+                          sx={{ margin: 2 }}
+                          multiline
+                          type="number"
+                          variant="filled"
+                          rows={1}
+                          value={time}
+                          onChange={handleChangeTime}
+                      />
+                  </Stack>
+                  <Button onClick={CloseDialog}>Close</Button>
+                  <Button onClick={EditTodo}>更新</Button>
+              </Dialog>
+          </Grid>
       </Layout>
   )
 }

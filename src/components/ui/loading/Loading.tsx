cac390e2c17a8dc89ae831b7a4e62c8de31ade13
nakazaaa <znaka791@gmail.com>
2023-05-09 01:10:04 +0900

    [Wip]ローディング画面実装

--- src/components/ui/loading/Loading.tsx
+++ src/components/ui/loading/Loading.tsx
@@ -3,13 +3,14 @@ import CircularProgress from '@mui/material/CircularProgress';
 import * as React from "react";
 import {useContext, useState} from "react";
 import {loadingContext} from "@/pages/_app";
-const loading = useContext(loadingContext);
+
 
 export  const Loading = () => {
+  const {open,setOpen} = useContext(loadingContext);
   return (
     <Backdrop
       sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
-      open={loading}
+      open={open}
     >
       <CircularProgress color="inherit" />
     </Backdrop>
